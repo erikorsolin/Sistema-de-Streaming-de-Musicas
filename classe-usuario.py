ff2250f91c7bc164ea367e4408b9a28f938f4ea2
erikorsolin <depaulaerik670@gmail.com>
2022-07-09 20:01:11 -0300

    exclusão da classe usuário

--- classe-usuario.py
+++
@@ -1,7 +0,0 @@
-from multiprocessing.reduction import send_handle
-
-
-class Usuario:
-    def __init__(self, username, senha, nome, sexo, data_nascimento, endereco_monetario, playlists_salvas):
-        self.username: username
-        self.senha: senha
