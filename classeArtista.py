bbe56b1400a17cd0e1f2b0ddce9cb1d4d386b53c
Andre Rocco <andre.amaral.rocco@gmail.com>
2022-07-11 18:35:05 -0300

    Argumentos atualizados

--- classeArtista.py
+++ classeArtista.py
@@ -4,8 +4,8 @@ from classeUsuario import Usuario
 
 # A classe Artista é uma subclasse de Usuário
 class Artista(Usuario):
-    def __init__(self, username, senha, nome, sexo, data_nascimento, nome_completo, numero_cartao, codigo_seguranca, data_validade, playlists_salvas=[]):
-        super().__init__(username, senha, nome, sexo, data_nascimento, nome_completo, numero_cartao, codigo_seguranca, data_validade, playlists_salvas)
+    def __init__(self, username, senha, nome, sexo, data_nascimento, nome_completo, numero_cartao, codigo_seguranca, data_validade):
+        super().__init__(username, senha, nome, sexo, data_nascimento, nome_completo, numero_cartao, codigo_seguranca, data_validade)
         self.seguidores = 0
         self.ranking = None
         self.musicas = []
