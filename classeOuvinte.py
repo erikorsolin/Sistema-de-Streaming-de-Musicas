043871da43b6227a3520cd9962268efa0e6dab9f
Andre Rocco <andre.amaral.rocco@gmail.com>
2022-07-11 18:35:25 -0300

    Argumentos atualizados

--- classeOuvinte.py
+++ classeOuvinte.py
@@ -2,8 +2,8 @@ from classeUsuario import Usuario
 
 # A classe Ouvinte é uma subclasse de Usuário
 class Ouvinte(Usuario):
-    def __init__(self, username, senha, nome, sexo, data_nascimento, nome_completo, numero_cartao, codigo_seguranca, data_validade, playlists_salvas=[]):
-        super().__init__(username, senha, nome, sexo, data_nascimento, nome_completo, numero_cartao, codigo_seguranca, data_validade, playlists_salvas)
+    def __init__(self, username, senha, nome, sexo, data_nascimento, nome_completo, numero_cartao, codigo_seguranca, data_validade):
+        super().__init__(username, senha, nome, sexo, data_nascimento, nome_completo, numero_cartao, codigo_seguranca, data_validade)
         self.artistas_seguindo = []
 
     def followArtista(self, lista_artistas): # O argumento lista_artistas é uma lista com todas as intâncias da classe artista
