f1044acc89eb2073404e10aa39d84101ca578260
Andre Rocco <andre.amaral.rocco@gmail.com>
2022-07-11 15:03:17 -0300

    Compatível com os métodos da classe Artista

--- classeMusica.py
+++ classeMusica.py
@@ -1,23 +1,10 @@
 class Musica:
     # Função construtora 
     def __init__(self, nome, artista, formato, duracao):
-        self.nome = nome
-        self.artista = artista
-        self.formato = formato
-        self.duracao = self.conversorSegundos(duracao)
-
-    # Função para converter o tempo da música
-    def conversorSegundos(tempo_min_segundo):
-        lista_min_segundo = tempo_min_segundo.split(':')
-        tempo_segundos = 0
-        for i, w in enumerate(lista_min_segundo):
-            if i == 0:
-                x = int(w)
-                tempo_segundos += (x*60)
-            else:
-                x = int(w)
-                tempo_segundos += x
-        return tempo_segundos
+        self.nome = nome # É uma string com o nome da música
+        self.artista = artista # Contém o nome do artista que gerou a música
+        self.formato = formato # É uma string "WAV", "MP3" ou "AAC"
+        self.duracao = duracao # É uma tupla no formato (mínutos, segundos)
 
     # Funções setters
     def setNomeCompleto(self, nome):
